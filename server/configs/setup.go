7857664727faaa95e89e52b07c80869c24fe2a0d
ottothorntonsilver <ottothorntonsilver+gh@github.com>
2023-07-25 11:16:26 -0700

    wip

---
+++ server/configs/setup.go
@@ -0,0 +1,47 @@
+package configs
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB() *mongo.Client {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	// adding defer function to handle error that occurs when disconnecting from MongoDB
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	//ping the database
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB")
+	return client
+}
+
+// Client instance
+var DB *mongo.Client = ConnectDB()
+
+// getting database collections
+func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	collection := client.Database("go-api-1").Collection(collectionName)
+	return collection
+}
--- server/main.go
+++ server/main.go
@@ -1,100 +1,30 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"net/http"
-	"time"
 
-	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"go-api-1/server/configs"
+
+	"github.com/labstack/echo/v4"
 )
-
-var db = make(map[string]string)
-var mongodb mongo.Client
-
-func setupRouter() *gin.Engine {
-	// Disable Console Color
-	// gin.DisableConsoleColor()
-	r := gin.Default()
-
-	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
-
-	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := db[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-		}
-	})
-
-	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo": "bar", // user:foo password:bar
-	}))
-
-	/* example curl for /admin with basicauth header
-	   Zm9vOmJhcg== is base64("foo:bar")
-
-		curl -X POST \
-	  	http://localhost:8080/admin \
-	  	-H 'authorization: Basic Zm9vOmJhcg==' \
-	  	-H 'content-type: application/json' \
-	  	-d '{"value":"bar"}'
-	*/
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			db[user] = json.Value
-
-			// coll := mongodb.Database("test").Collection("users")
-			// var existing_user bson.M
-
-			c.JSON(http.StatusOK, gin.H{"status": "ok"})
-		}
-	})
-
-	return r
-}
 
 func main() {
 	fmt.Println("Starting our application...")
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+	e := echo.New()
+	configs.ConnectDB()
 
-	fmt.Println("Connecting to MongoDB...")
-	mongodb, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		panic(err)
-	}
+	e.GET("/ping", func(c echo.Context) error {
+		return c.String(http.StatusOK, "pong")
+	})
 
-	if err != nil {
-		panic(err)
-	}
+	e.POST("/events", func(c echo.Context) error {
+		return echo.ErrNotImplemented
+	})
 
-	fmt.Println("Connected to MongoDB!")
+	e.GET("/events", func(c echo.Context) error {
+		return echo.ErrNotImplemented
+	})
 
-	// adding defer function to handle error that occurs when disconnecting from MongoDB
-	defer func() {
-		if err = mongodb.Disconnect(ctx); err != nil {
-			panic(err)
-		}
-	}()
-
-	fmt.Println("Serving GIN REST API...")
-	r := setupRouter()
-	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	e.Logger.Fatal(e.Start(":8080"))
 }
