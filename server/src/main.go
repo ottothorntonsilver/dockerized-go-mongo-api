f7c61d9812789a30fd20322791332d2d9882f589
ottothorntonsilver <ottothorntonsilver+gh@github.com>
2023-07-26 11:22:40 -0700

    getting 500

--- server/src/main.go
+++ server/src/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"go-api-1/server/configs"
+	"go-api-1/server/models"
+	"go-api-1/server/responses"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func main() {
 	fmt.Println("Starting our application...")
 	e := echo.New()
-	configs.ConnectDB()
+	DB := configs.ConnectDB()
 
 	e.GET("/ping", func(c echo.Context) error {
 		return c.String(http.StatusOK, "pong")
@@ -24,22 +29,38 @@ func main() {
 		TicketCount int    `json:"ticket_count"`
 	}
 
-	// e.POST("/events", func(c echo.Context) error {
-	// 	event := new(event_schema)
-	// 	if err := c.Bind(event); err != nil {
-	// 		return c.String(http.StatusBadRequest, "bad request")
-	// 	}
+	e.POST("/events", func(c echo.Context) error {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		event := new(event_schema)
+		if err := c.Bind(&event); err != nil {
+			return c.String(http.StatusBadRequest, "bad request")
+		}
 
-	// 	fmt.Println(event)
+		event_model := models.Event{
+			Id:          primitive.NewObjectID(),
+			Name:        event.Name,
+			VenueID:     event.VenueID,
+			Date:        event.Date,
+			TicketCount: event.TicketCount,
+		}
 
-	// 	res, err := DB.Collection("events").InsertOne(models.Event{event})
+		res, err := configs.GetCollection(DB, "events").InsertOne(ctx, event_model)
 
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
+		if err != nil {
+			return c.JSON(
+				http.StatusInternalServerError,
+				responses.DefaultResponse{
+					Status:  http.StatusInternalServerError,
+					Message: "error",
+					Data:    &echo.Map{"data": err.Error()},
+				},
+			)
 
-	// 	return c.JSON(http.StatusCreated, res)
-	// })
+		}
+
+		return c.JSON(http.StatusCreated, res)
+	})
 
 	e.GET("/events", func(c echo.Context) error {
 		return echo.ErrNotImplemented
--- server/src/models/event.go
+++ server/src/models/event.go
@@ -1,6 +1,9 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Event struct {
+	Id          primitive.ObjectID `json:"id,omitempty"`
 	Name        string
 	VenueID     string `bson:"venue_id,omitempty"`
 	Date        string `bson:"date,omitempty"`
